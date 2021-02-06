38fa044af72ca539621633755fe57e438af85d1c
Darkjeff <jeff@jeffinfo.com>
2021-02-06 16:02:01 +0100

    add new stats

---
+++ class/pickingstats.class.php
@@ -0,0 +1,189 @@
+<?php
+/* Copyright (C) 2003      Rodolphe Quiedeville <rodolphe@quiedeville.org>
+ * Copyright (c) 2005-2013 Laurent Destailleur  <eldy@users.sourceforge.net>
+ * Copyright (C) 2005-2012 Regis Houssin        <regis.houssin@inodbox.com>
+ * Copyright (C) 2012      Marcos García        <marcosgdf@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+
+include_once DOL_DOCUMENT_ROOT.'/core/class/stats.class.php';
+include_once DOL_DOCUMENT_ROOT.'/core/lib/date.lib.php';
+dol_include_once('/monanalysevendeur/class/rapportjournalier.class.php');
+
+/**
+ *    Class to manage intervention statistics
+ */
+class MonAnayseVendeurStats
+{
+    /**
+     * @var string Name of table without prefix where object is stored
+     */
+    public $table_element;
+
+    public $data_traitement;
+    public $data_transfo;
+    public $data_row;
+    public $data_legend;
+
+    /**
+     * Constructor
+     *
+     * @param 	DoliDB	$db		   Database handler
+     */
+    public function __construct($db)
+    {
+        global $user, $conf;
+
+        $this->data_traitement=array();
+		$this->data_transfo=array();
+		$this->data_row=array();
+		$this->data_legend=array();
+
+        $this->db = $db;
+    }
+
+    public function getData($users, $user_tags, $period_type, $from_date, $to_date) {
+
+    	if ($period_type=='day') {
+			$result = $this->getNb($users, $user_tags, '%d/%m/%Y', $from_date, $to_date);
+		}
+		if ($period_type=='week') {
+			$result = $this->getNb($users, $user_tags, '%U', $from_date, $to_date);
+		}
+		if ($period_type=='month') {
+			$result = $this->getNb($users, $user_tags, '%m', $from_date, $to_date);
+		}
+		return $result;
+	}
+
+	/**
+	 * Return intervention number by month for a year
+	 *
+	 * @param	int		$year		Year to scan
+	 *	@param	int		$format		0=Label of abscissa is a translated text, 1=Label of abscissa is month number, 2=Label of abscissa is first letter of month
+	 * @return	array				Array with number by month
+	 */
+	public function getNb($users, $user_tags, $period_type, $from_date, $to_date)
+	{
+		global $user;
+		$object_static=new Rapportjournalier($this->db);
+		$sql_where=array();
+		$nbday_between=num_between_day($from_date, $to_date, 1);
+
+
+		if ($period_type=='%d/%m/%Y') {
+			$nbtime_diff=$nbday_between;
+
+			$time_array=array();
+			for ($i = 0; $i <= $nbtime_diff; $i++)
+			{
+				$time_array[$i]=dol_print_date(dol_time_plus_duree($from_date,$i,'d'),'%d/%m/%Y');
+			}
+		} elseif($period_type=='%U') {
+			$nbtime_diff=$nbday_between/7;
+			//TODO format week
+			$time_array=array();
+			for ($i = 0; $i <= $nbtime_diff; $i++)
+			{
+				$time_array[$i]=dol_print_date(dol_time_plus_duree($from_date,$i,'w'),'%U');
+			}
+		} elseif($period_type=='%m') {
+			$nbtime_diff=$nbday_between/30;
+			$time_array=array();
+			for ($i = 0; $i <= $nbtime_diff; $i++)
+			{
+				$time_array[$i]=dol_print_date(dol_time_plus_duree($from_date,$i,'m'),'%m');
+			}
+		}
+
+		$sql = "SELECT date_format(t.date_creation,'".$period_type."') as dm, t.fk_user_creat, SUM(t.nb_traitement) as nb, SUM(t.nb_box) as nbbox";
+		$sql .= " FROM ".MAIN_DB_PREFIX.$object_static->table_element . ' as t';
+		if (!empty($user_tags)) {
+			$sql .= ' INNER JOIN llx_categorie_user as tagu ON tagu.fk_user=t.fk_user_creat';
+		}
+		$sql_where[] = " t.date_creation BETWEEN '".$this->db->idate($from_date)."' AND '".$this->db->idate($to_date)."'";
+		if (!empty($users)) {
+			$sql_where[] = ' t.fk_user_creat IN (' . implode(',', $users) . ')';
+		}
+		if (!empty($user_tags)) {
+			$sql_where[] = ' tagu.fk_categorie IN (' . implode(',', $user_tags) . ')';
+
+		}
+		if (!empty($sql_where)) {
+			$sql .= ' WHERE '.implode( ' AND ',$sql_where);
+		}
+
+		$sql .= " GROUP BY dm, t.fk_user_creat";
+		$sql .= $this->db->order('dm,t.fk_user_creat', 'DESC');
+
+
+		$result=array();
+		$resql = $this->db->query($sql);
+
+		if ($resql)
+		{
+			$num = $this->db->num_rows($resql);
+			$i=0;
+			while ($row = $this->db->fetch_row($resql))
+			{
+				$result[$i] = $row;
+				$i++;
+			}
+		} else {
+			return -1;
+			$this->error=$this->db->lasterror;
+		}
+
+		$user_data=array();
+		$i=1;
+		foreach($result as $time=>$data) {
+			if (!in_array($data[1],$user_data)) {
+				$user_data[$i] = $data[1];
+				$i++;
+			}
+		}
+
+
+		$data_r=array();
+		foreach($time_array as $i=>$time) {
+			$t[0]=$time;
+			$u=1;
+			foreach($user_data as $userid) {
+				$t[$u]=0;
+				$u++;
+			}
+			$data_r[]=$t;
+		}
+
+		$data_tx=$data_r;
+		foreach($data_r as $i=>$data_st) {
+			foreach($result as $data_src) {
+				if ($data_src[0]==$data_st[0]) {
+					$data_r[$i][array_search($data_src[1], $user_data)]=$data_src[2];
+					if ($data_src[2]!=0) {
+						$data_tx[$i][array_search($data_src[1], $user_data)] = round(($data_src[3] / $data_src[2]) * 100);
+					}
+				}
+			}
+		}
+
+		$this->data_traitement = $data_r;
+		$this->data_transfo = $data_tx;
+		$this->data_row = $result;
+		$this->data_legend = $user_data;
+		return 1;
+	}
+}
---
+++ picking_stat.php
@@ -0,0 +1,228 @@
+<?php
+/* Copyright (C) 2016 Laurent Destailleur  <eldy@users.sourceforge.net>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
+ *        \file       htdocs/fichinter/stats/index.php
+ *      \ingroup    fichinter
+ *        \brief      Page with interventions statistics
+ */
+
+require '../../main.inc.php';
+require_once DOL_DOCUMENT_ROOT . '/core/class/dolgraph.class.php';
+
+dol_include_once('/monanalysevendeur/class/monanalysevendeurstats.class.php');
+
+$WIDTH = DolGraph::getDefaultGraphSizeForStats('width');
+$HEIGHT = DolGraph::getDefaultGraphSizeForStats('height');
+
+$mode = 'customer';
+if (!$user->rights->monanalysevendeur->read) accessforbidden();
+
+$users = GETPOST('usersid', 'array');
+$user_tags = GETPOST('users_tags', 'array');
+$period_type = GETPOST('period_type', 'alpha');
+$from_date = dol_mktime(0, 0, 0, GETPOST('frmdtmonth', 'int'), GETPOST('frmdtday', 'int'), GETPOST('frmdtyear', 'int'));
+$to_date = dol_mktime(23, 59, 59, GETPOST('todtmonth', 'int'), GETPOST('todtday', 'int'), GETPOST('todtyear', 'int'));
+
+$nowyear = strftime("%Y", dol_now());
+$year = GETPOST('year') > 0 ? GETPOST('year', 'int') : $nowyear;
+
+// Load translation files required by the page
+$langs->loadLangs(array('monanalysevendeur@monanalysevendeur', 'other'));
+
+
+/*
+ * View
+ */
+
+$form = new Form($db);
+
+$title = $langs->trans("StatMonAnayseVendeurStatistics");
+$dir = $conf->monanalysevendeur->dir_temp;
+
+llxHeader('', $title);
+
+print load_fiche_titre($title, '', 'intervention');
+
+dol_mkdir($dir);
+
+$stats = new MonAnayseVendeurStats($db);
+
+// Build graphic number of object
+$result = $stats->getData($users, $user_tags, $period_type, $from_date, $to_date);
+if ($result < 0) {
+	setEventMessage($stats->error, 'errors');
+}
+
+$data_traitement = $stats->data_traitement;
+$data_transfo = $stats->data_transfo;
+$legend = array();
+foreach ($stats->data_legend as $i => $u) {
+	$user_static = new User($db);
+	$user_static->fetch($u);
+	$legend[] = $user_static->getFullName($langs);
+}
+
+$filenamenb = $dir . '/monanalysevendeur' . $period_type . '-' . hash('md5', implode(',', $users)) . hash('md5', implode(',', $user_tags)) . '-' . $from_date . '-' . $to_date . '.png';
+$fileurlnb = DOL_URL_ROOT . '/viewimage.php?modulepart=monanalysevendeurstats&file=monanalysevendeur' . $period_type . '-' . hash('md5', implode(',', $users)) . hash('md5', implode(',', $user_tags)) . '-' . $from_date . '-' . $to_date . '.png';
+
+$px1 = new DolGraph();
+$mesg = $px1->isGraphKo();
+if (!$mesg && !empty($data_traitement)) {
+	$px1->SetData($data_traitement);
+	$px1->SetLegend($legend);
+	$px1->SetMaxValue($px1->GetCeilMaxValue());
+	$px1->SetMinValue(min(0, $px1->GetFloorMinValue()));
+	$px1->SetWidth($WIDTH);
+	$px1->SetHeight($HEIGHT);
+	$px1->SetYLabel($langs->trans("Nombre Traitement"));
+	$px1->SetShading(3);
+	$px1->SetHorizTickIncrement(1);
+	$px1->mode = 'depth';
+	$px1->SetTitle($langs->trans("Nombre Traitement"));
+
+	$px1->draw($filenamenb, $fileurlnb);
+}
+
+$filenamenbtx = $dir . '/monanalysevendeurtx' . $period_type . '-' . hash('md5', implode(',', $users)) . hash('md5', implode(',', $user_tags)) . '-' . $from_date . '-' . $to_date . '.png';
+$fileurlnbtx = DOL_URL_ROOT . '/viewimage.php?modulepart=monanalysevendeurstats&file=monanalysevendeurtx' . $period_type . '-' . hash('md5', implode(',', $users)) . hash('md5', implode(',', $user_tags)) . '-' . $from_date . '-' . $to_date . '.png';
+
+$px2 = new DolGraph();
+$mesg = $px2->isGraphKo();
+if (!$mesg && !empty($data_transfo)) {
+	$px2->SetData($data_transfo);
+	$px2->SetLegend($legend);
+	$px2->SetMaxValue($px2->GetCeilMaxValue());
+	$px2->SetMinValue(min(0, $px2->GetFloorMinValue()));
+	$px2->SetWidth($WIDTH);
+	$px2->SetHeight($HEIGHT);
+	$px2->SetYLabel($langs->trans("Tx Transformation"));
+	$px2->SetShading(3);
+	$px2->SetHorizTickIncrement(1);
+	$px2->mode = 'depth';
+	$px2->SetTitle($langs->trans("Tx Transformation"));
+
+	$px2->draw($filenamenbtx, $fileurlnbtx);
+}
+
+$h = 0;
+$head = array();
+$head[$h][0] = dol_buildpath("/monanalysevendeur/index_stat.php", 1);
+$head[$h][1] = $langs->trans("Statistics");
+$head[$h][2] = 'stats';
+$h++;
+
+//$type = 'fichinter_stats';
+
+//complete_head_from_modules($conf, $langs, null, $head, $h, $type);
+
+dol_fiche_head($head, 'stats', $langs->trans("Statistics"), -1);
+
+
+print '<div class="fichecenter"><div class="fichethirdleft">';
+
+
+//if (empty($socid))
+//{
+// Show filter box
+print '<form name="stats" method="POST" action="' . $_SERVER["PHP_SELF"] . '">';
+print '<input type="hidden" name="token" value="' . newToken() . '">';
+print '<input type="hidden" name="mode" value="' . $mode . '">';
+
+print '<table class="noborder centpercent">';
+print '<tr class="liste_titre"><td class="liste_titre" colspan="2">' . $langs->trans("Filter") . '</td></tr>';
+// Users
+print '<tr><td class="left">' . $langs->trans("Users") . '</td><td class="left">';
+$userlist = $form->select_dolusers('', '', 0, null, 0, '', '', 0, 0, 0, 'AND u.statut = 1', 0, '', '', 0, 1);
+// Note: If user has no right to "see all thirdparties", we force selection of sale representative to him, so after creation he can see the record.
+print $form->multiselectarray('usersid', $userlist, $users, null, null, null, null, "90%");
+print '</td></tr>';
+// Categorie users
+print '<tr><td class="left">' . $langs->trans("Categories") . '</td><td class="left">';
+$cate_arbo = $form->select_all_categories('user', null, 'parent', null, null, 1);
+print $form->multiselectarray('users_tags', $cate_arbo, $user_tags, null, null, null, null, '90%');
+// Periode Type
+print '<tr><td class="left">' . $langs->trans("Period") . '</td><td class="left">';
+$type_period_array = array('day' => $langs->trans('Day'),
+	'week'=>$langs->trans('Week'),
+	'month' => $langs->trans('Month'));
+print $form->selectarray('period_type', $type_period_array, $period_type, 1, 0, 0, '', 1);
+print '</td></tr>';
+// From Dt To Dt
+print '<tr><td class="left">' . $langs->trans("From") . '</td><td class="left">';
+print $form->selectDate($from_date, 'frmdt', 0, 0, 1, 'stats', 1, 0);
+print '</td></tr>';
+print '<tr><td class="left">' . $langs->trans("To") . '</td><td class="left">';
+print $form->selectDate($to_date, 'todt', 0, 0, 1, 'stats', 1, 0);
+print '</td></tr>';
+print '<tr><td class="center" colspan="2"><input type="submit" name="submit" class="button" value="' . $langs->trans("Refresh") . '"></td></tr>';
+print '</table>';
+print '</form>';
+print '<br><br>';
+//}
+
+print '<div class="div-table-responsive-no-min">';
+print '<table class="noborder centpercent">';
+print '<tr class="liste_titre" height="24">';
+print '<td class="center">' . $langs->trans($type_period_array[$period_type]) . '</td>';
+print '<td class="center">' . $langs->trans('User') . '</td>';
+print '<td class="right">' . $langs->trans("Nombre Traitement") . '</td>';
+print '<td class="right">' . $langs->trans("Tx Transformation") . '</td>';
+print '</tr>';
+
+
+foreach ($stats->data_row as $val) {
+
+	print '<tr class="oddeven">';
+	print '<td class="center">' . $val[0] . '</td>';
+	$user_static = new User($db);
+	$user_static->fetch($val[1]);
+	print '<td class="center">' . $user_static->getFullName($langs). '</td>';
+	print '<td class="right">' . $val[2] . '</td>';
+	print '<td class="right">' . ($val[2]!=0?round(($val[3] / $val[2]) * 100):'') . '</td>';
+	print '</tr>';
+}
+
+print '</table>';
+print '</div>';
+
+
+print '</div><div class="fichetwothirdright"><div class="ficheaddleft">';
+
+
+// Show graphs
+print '<table class="border centpercent"><tr class="pair nohover"><td class="center">';
+if ($mesg) {
+	print $mesg;
+} else {
+	print $px1->show();
+	print "<br>\n";
+	print $px2->show();
+}
+print '</td></tr></table>';
+
+
+print '</div></div></div>';
+print '<div style="clear:both"></div>';
+
+dol_fiche_end();
+
+
+llxFooter();
+
+$db->close();
+
