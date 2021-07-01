494c0e62f2d1b1a8cfba47c0ecb3396026711d9d
Florian HENRY <florian.henry@open-concept.pro>
2021-07-01 15:24:36 +0200

    add link to evt list

--- core/modules/modMonAnalyseVendeur.class.php
+++ core/modules/modMonAnalyseVendeur.class.php
@@ -619,6 +619,26 @@ class modMonAnalyseVendeur extends DolibarrModules
             'user'=>2
         );
 		$this->menu[$r++]=array(
+			// '' if this is a top menu. For left menu, use 'fk_mainmenu=xxx' or 'fk_mainmenu=xxx,fk_leftmenu=yyy' where xxx is mainmenucode and yyy is a leftmenucode
+			'fk_menu'=>'fk_mainmenu=monanalysevendeur,fk_leftmenu=monanalysevendeur_relance',
+			// This is a Left menu entry
+			'type'=>'left',
+			'titre'=>'Liste des Evt de Relance',
+			'mainmenu'=>'monanalysevendeur',
+			'leftmenu'=>'monanalysevendeur_relancelistevt',
+			'url'=>'/comm/action/list.php?sortfield=c.libelle&sortorder=desc&begin=&contextpage=actioncommlist&search_actioncode=AC_OTH&search_filtert=-1&search_title=Relance',
+			// Lang file to use (without .lang) by module. File must be in langs/code_CODE/ directory.
+			'langs'=>'monanalysevendeur@monanalysevendeur',
+			'position'=>1100+$r,
+			// Define condition to show or hide menu entry. Use '$conf->monanalysevendeur->enabled' if entry must be visible if module is enabled. Use '$leftmenu==\'system\'' to show if leftmenu system is selected.
+			'enabled'=>'$conf->monanalysevendeur->enabled',
+			// Use 'perms'=>'$user->rights->monanalysevendeur->level1->level2' if you want your menu with a permission rules
+			'perms'=>'1',
+			'target'=>'',
+			// 0=Menu for internal users, 1=external users, 2=both
+			'user'=>2
+		);
+		$this->menu[$r++]=array(
             // '' if this is a top menu. For left menu, use 'fk_mainmenu=xxx' or 'fk_mainmenu=xxx,fk_leftmenu=yyy' where xxx is mainmenucode and yyy is a leftmenucode
             'fk_menu'=>'fk_mainmenu=monanalysevendeur,fk_leftmenu=monanalysevendeur_relance',
             // This is a Left menu entry
