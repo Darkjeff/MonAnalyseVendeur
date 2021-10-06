1d82cb4fc78802ddf2ec2cd012e6b4016776007b
Florian HENRY <florian.henry@open-concept.pro>
2021-10-06 17:43:05 +0200

    ok for evt on change st prospect

---
+++ class/actions_monanalysevendeur.class.php
@@ -0,0 +1,345 @@
+<?php
+/* Copyright (C) 2021 SuperAdmin
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
+ * \file    monanalysevendeur/class/actions_monanalysevendeur.class.php
+ * \ingroup monanalysevendeur
+ * \brief   Example hook overload.
+ *
+ * Put detailed description here.
+ */
+
+/**
+ * Class ActionsMonAnalyseVendeur
+ */
+class ActionsMonAnalyseVendeur
+{
+	/**
+	 * @var DoliDB Database handler.
+	 */
+	public $db;
+
+	/**
+	 * @var string Error code (or message)
+	 */
+	public $error = '';
+
+	/**
+	 * @var array Errors
+	 */
+	public $errors = array();
+
+
+	/**
+	 * @var array Hook results. Propagated to $hookmanager->resArray for later reuse
+	 */
+	public $results = array();
+
+	/**
+	 * @var string String displayed by executeHook() immediately after return
+	 */
+	public $resprints;
+
+
+	/**
+	 * Constructor
+	 *
+	 *  @param		DoliDB		$db      Database handler
+	 */
+	public function __construct($db)
+	{
+		$this->db = $db;
+	}
+
+
+	/**
+	 * Execute action
+	 *
+	 * @param	array			$parameters		Array of parameters
+	 * @param	CommonObject    $object         The object to process (an invoice if you are in invoice module, a propale in propale's module, etc...)
+	 * @param	string			$action      	'add', 'update', 'view'
+	 * @return	int         					<0 if KO,
+	 *                           				=0 if OK but we want to process standard actions too,
+	 *                            				>0 if OK and we want to replace standard actions.
+	 */
+	public function getNomUrl($parameters, &$object, &$action)
+	{
+		global $db, $langs, $conf, $user;
+		$this->resprints = '';
+		return 0;
+	}
+
+	/**
+	 * Overloading the doActions function : replacing the parent's function with the one below
+	 *
+	 * @param   array           $parameters     Hook metadatas (context, etc...)
+	 * @param   CommonObject    $object         The object to process (an invoice if you are in invoice module, a propale in propale's module, etc...)
+	 * @param   string          $action         Current action (if set). Generally create or edit or null
+	 * @param   HookManager     $hookmanager    Hook manager propagated to allow calling another hook
+	 * @return  int                             < 0 on error, 0 on success, 1 to replace standard code
+	 */
+	public function doActions($parameters, &$object, &$action, $hookmanager)
+	{
+		global $conf, $user, $langs;
+
+		$error = 0; // Error counter
+
+		/* print_r($parameters); print_r($object); echo "action: " . $action; */
+		if (in_array($parameters['currentcontext'], array('thirdpartylist', 'thirdpartycomm')))	    // do something only for the context 'somecontext1' or 'somecontext2'
+		{
+			if ($action=='setstcomm') {
+
+				if (empty($object->id)) {
+					$socid = GETPOST('stcommsocid','int');
+				} else {
+					$socid = $object->id;
+				}
+				$stcomm_id=dol_getIdFromCode($this->db, GETPOST('stcomm', 'alpha'), 'c_stcomm');
+
+				$now=dol_now();
+				require_once DOL_DOCUMENT_ROOT.'/comm/action/class/actioncomm.class.php';
+				$actioncomm = new ActionComm($this->db);
+				$actioncomm->type_code   = 'AC_OTH_AUTO'; // Type of event ('AC_OTH', 'AC_OTH_AUTO', 'AC_XXX'...)
+				$actioncomm->code        = 'AC_OTH_AUTO';
+				$actioncomm->label       = 'Status de prospection passé à '.$langs->trans('StatusProspect'.$stcomm_id);
+				//$actioncomm->note_private = $object->actionmsg; // TODO Replace with ($actioncomm->email_msgid ? $object->email_content : $object->actionmsg)
+				//$actioncomm->fk_project  = $projectid;
+				$actioncomm->datep       = $now;
+				$actioncomm->datef       = $now;
+				$actioncomm->durationp   = 0;
+				$actioncomm->percentage  = -1; // Not applicable
+				$actioncomm->socid       = $socid;
+				$actioncomm->authorid    = $user->id; // User saving action
+				$actioncomm->userownerid = $user->id; // Owner of action
+
+				$actioncomm->fk_element  = $socid;
+				$actioncomm->elementtype = 'societe';
+
+				$ret = $actioncomm->create($user); // User creating action
+
+				if ($ret < 0) {
+					$error++;
+					$this->errors[]=$actioncomm->error;
+				}
+
+
+			}
+		}
+
+		if (!$error) {
+			$this->results = array('myreturn' => 999);
+			$this->resprints = 'A text to show';
+			return 0; // or return 1 to replace standard code
+		} else {
+			return -1;
+		}
+	}
+
+
+	/**
+	 * Overloading the doMassActions function : replacing the parent's function with the one below
+	 *
+	 * @param   array           $parameters     Hook metadatas (context, etc...)
+	 * @param   CommonObject    $object         The object to process (an invoice if you are in invoice module, a propale in propale's module, etc...)
+	 * @param   string          $action         Current action (if set). Generally create or edit or null
+	 * @param   HookManager     $hookmanager    Hook manager propagated to allow calling another hook
+	 * @return  int                             < 0 on error, 0 on success, 1 to replace standard code
+	 */
+	public function doMassActions($parameters, &$object, &$action, $hookmanager)
+	{
+		global $conf, $user, $langs;
+
+		$error = 0; // Error counter
+
+		/* print_r($parameters); print_r($object); echo "action: " . $action; */
+		if (in_array($parameters['currentcontext'], array('somecontext1', 'somecontext2')))		// do something only for the context 'somecontext1' or 'somecontext2'
+		{
+			foreach ($parameters['toselect'] as $objectid)
+			{
+				// Do action on each object id
+			}
+		}
+
+		if (!$error) {
+			$this->results = array('myreturn' => 999);
+			$this->resprints = 'A text to show';
+			return 0; // or return 1 to replace standard code
+		} else {
+			$this->errors[] = 'Error message';
+			return -1;
+		}
+	}
+
+
+	/**
+	 * Overloading the addMoreMassActions function : replacing the parent's function with the one below
+	 *
+	 * @param   array           $parameters     Hook metadatas (context, etc...)
+	 * @param   CommonObject    $object         The object to process (an invoice if you are in invoice module, a propale in propale's module, etc...)
+	 * @param   string          $action         Current action (if set). Generally create or edit or null
+	 * @param   HookManager     $hookmanager    Hook manager propagated to allow calling another hook
+	 * @return  int                             < 0 on error, 0 on success, 1 to replace standard code
+	 */
+	public function addMoreMassActions($parameters, &$object, &$action, $hookmanager)
+	{
+		global $conf, $user, $langs;
+
+		$error = 0; // Error counter
+
+		/* print_r($parameters); print_r($object); echo "action: " . $action; */
+		if (in_array($parameters['currentcontext'], array('somecontext1', 'somecontext2')))		// do something only for the context 'somecontext1' or 'somecontext2'
+		{
+			$this->resprints = '<option value="0"'.($disabled ? ' disabled="disabled"' : '').'>'.$langs->trans("MonAnalyseVendeurMassAction").'</option>';
+		}
+
+		if (!$error) {
+			return 0; // or return 1 to replace standard code
+		} else {
+			$this->errors[] = 'Error message';
+			return -1;
+		}
+	}
+
+
+
+	/**
+	 * Execute action
+	 *
+	 * @param	array	$parameters     Array of parameters
+	 * @param   Object	$object		   	Object output on PDF
+	 * @param   string	$action     	'add', 'update', 'view'
+	 * @return  int 		        	<0 if KO,
+	 *                          		=0 if OK but we want to process standard actions too,
+	 *  	                            >0 if OK and we want to replace standard actions.
+	 */
+	public function beforePDFCreation($parameters, &$object, &$action)
+	{
+		global $conf, $user, $langs;
+		global $hookmanager;
+
+		$outputlangs = $langs;
+
+		$ret = 0; $deltemp = array();
+		dol_syslog(get_class($this).'::executeHooks action='.$action);
+
+		/* print_r($parameters); print_r($object); echo "action: " . $action; */
+		if (in_array($parameters['currentcontext'], array('somecontext1', 'somecontext2')))		// do something only for the context 'somecontext1' or 'somecontext2'
+		{
+		}
+
+		return $ret;
+	}
+
+	/**
+	 * Execute action
+	 *
+	 * @param	array	$parameters     Array of parameters
+	 * @param   Object	$pdfhandler     PDF builder handler
+	 * @param   string	$action         'add', 'update', 'view'
+	 * @return  int 		            <0 if KO,
+	 *                                  =0 if OK but we want to process standard actions too,
+	 *                                  >0 if OK and we want to replace standard actions.
+	 */
+	public function afterPDFCreation($parameters, &$pdfhandler, &$action)
+	{
+		global $conf, $user, $langs;
+		global $hookmanager;
+
+		$outputlangs = $langs;
+
+		$ret = 0; $deltemp = array();
+		dol_syslog(get_class($this).'::executeHooks action='.$action);
+
+		/* print_r($parameters); print_r($object); echo "action: " . $action; */
+		if (in_array($parameters['currentcontext'], array('somecontext1', 'somecontext2')))		// do something only for the context 'somecontext1' or 'somecontext2'
+		{
+		}
+
+		return $ret;
+	}
+
+
+
+	/**
+	 * Overloading the loadDataForCustomReports function : returns data to complete the customreport tool
+	 *
+	 * @param   array           $parameters     Hook metadatas (context, etc...)
+	 * @param   string          $action         Current action (if set). Generally create or edit or null
+	 * @param   HookManager     $hookmanager    Hook manager propagated to allow calling another hook
+	 * @return  int                             < 0 on error, 0 on success, 1 to replace standard code
+	 */
+	public function loadDataForCustomReports($parameters, &$action, $hookmanager)
+	{
+		global $conf, $user, $langs;
+
+		$langs->load("monanalysevendeur@monanalysevendeur");
+
+		$this->results = array();
+
+		$head = array();
+		$h = 0;
+
+		if ($parameters['tabfamily'] == 'monanalysevendeur') {
+			$head[$h][0] = dol_buildpath('/module/index.php', 1);
+			$head[$h][1] = $langs->trans("Home");
+			$head[$h][2] = 'home';
+			$h++;
+
+			$this->results['title'] = $langs->trans("MonAnalyseVendeur");
+			$this->results['picto'] = 'monanalysevendeur@monanalysevendeur';
+		}
+
+		$head[$h][0] = 'customreports.php?objecttype='.$parameters['objecttype'].(empty($parameters['tabfamily']) ? '' : '&tabfamily='.$parameters['tabfamily']);
+		$head[$h][1] = $langs->trans("CustomReports");
+		$head[$h][2] = 'customreports';
+
+		$this->results['head'] = $head;
+
+		return 1;
+	}
+
+
+
+	/**
+	 * Overloading the restrictedArea function : check permission on an object
+	 *
+	 * @param   array           $parameters     Hook metadatas (context, etc...)
+	 * @param   string          $action         Current action (if set). Generally create or edit or null
+	 * @param   HookManager     $hookmanager    Hook manager propagated to allow calling another hook
+	 * @return  int 		      			  	<0 if KO,
+	 *                          				=0 if OK but we want to process standard actions too,
+	 *  	                            		>0 if OK and we want to replace standard actions.
+	 */
+	public function restrictedArea($parameters, &$action, $hookmanager)
+	{
+		global $user;
+
+		if ($parameters['features'] == 'myobject') {
+			if ($user->rights->monanalysevendeur->myobject->read) {
+				$this->results['result'] = 1;
+				return 1;
+			} else {
+				$this->results['result'] = 0;
+				return 1;
+			}
+		}
+
+		return 0;
+	}
+
+	/* Add here any other hooked methods... */
+}
--- core/modules/modMonAnalyseVendeur.class.php
+++ core/modules/modMonAnalyseVendeur.class.php
@@ -102,10 +102,10 @@ class modMonAnalyseVendeur extends DolibarrModules
 			),
 			// Set here all hooks context managed by module. To find available hook context, make a "grep -r '>initHooks(' *" on source code. You can also set hook context to 'all'
 			'hooks' => array(
-				//   'data' => array(
-				//       'hookcontext1',
-				//       'hookcontext2',
-				//   ),
+				   'data' => array(
+				       'thirdpartylist',
+				       'thirdpartycomm',
+				   ),
 				//   'entity' => '0',
 			),
 			// Set this to 1 if features of module are opened to external users
