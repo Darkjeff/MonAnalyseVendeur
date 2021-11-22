8a1cd7c199554d5acb7d4d264f1101b71e020f4b
Darkjeff <jeff@jeffinfo.com>
2021-11-22 14:10:22 +0100

    correct sql

--- class/monanalysemagstats.class.php
+++ class/monanalysemagstats.class.php
@@ -1,4 +1,4 @@
-﻿<?php
+<?php
 /* Copyright (C) 2003      Rodolphe Quiedeville <rodolphe@quiedeville.org>
  * Copyright (c) 2005-2013 Laurent Destailleur  <eldy@users.sourceforge.net>
  * Copyright (C) 2005-2012 Regis Houssin        <regis.houssin@inodbox.com>
@@ -196,7 +196,7 @@ class MonAnayseVendeurStats
 			//AutoDiag
 			$sql = 'SELECT count(ct.rowid) as nb FROM ' . MAIN_DB_PREFIX . 'contacttracking as ct';
 			$sql .= ' INNER JOIN ' . MAIN_DB_PREFIX . 'categorie_user as catu ON ct.fk_user_creat=catu.fk_user ';
-			$sql .= " WHERE ac.datec BETWEEN '".$this->db->idate($from_date)."' AND '".$this->db->idate($to_date)."'";
+			$sql .= " WHERE ct.date_creation BETWEEN '".$this->db->idate($from_date)."' AND '".$this->db->idate($to_date)."'";
 			$sql .= ' AND ct.fk_event is null';
 			$sql .= ' AND catu.fk_categorie='.$key;
 			$resql = $this->db->query($sql);
@@ -215,8 +215,8 @@ class MonAnayseVendeurStats
 			$sql = 'SELECT count(ske.rowid) as nbd FROM ' . MAIN_DB_PREFIX . 'monanalysevendeur_sketchsvendeur as ske';
 			$sql .= ' INNER JOIN ' . MAIN_DB_PREFIX . 'categorie_user as catu ON ske.fk_user=catu.fk_user ';
 			$sql .= " WHERE ske.date BETWEEN '".$this->db->idate($from_date)."' AND '".$this->db->idate($to_date)."'";
-			$sql .= ' AND catu.fk_category_user='.$key;
-			$sql .= " GROUP BY catu.fk_category_user";
+			$sql .= ' AND catu.fk_categorie='.$key;
+			$sql .= " GROUP BY catu.fk_categorie";
 			$resql = $this->db->query($sql);
 			if ($resql)
 			{
